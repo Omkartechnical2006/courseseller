3048eaac3b064a24d488942b0844b9e3fd17d9b1
omkar <omkar2006suman@gmail.com>
2025-04-09 11:51:19 +0530

    added responsive credentials page and popup msg for updation of success

--- courswithnodeapp/middleware/adminAuth.js
+++
@@ -1,9 +0,0 @@
-const isAdmin = (req, res, next) => {
-    if (req.session && req.session.isAdmin) {
-        next();
-    } else {
-        res.redirect('/admin/login');
-    }
-};
-
-module.exports = isAdmin; 
